0fc3da535e8030a0c5b9755151aefd6f3be65b3c
mahesh-tippanu <maheshkumartippanu@gmail.com>
2022-05-06 22:00:38 +0500

    initial commit

---
+++ src/linkedlist/LinkedListMain.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class LinkedListMain {
+
+        public static void main(String[] args) {
+
+            Simple_list obj = new Simple_list();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            System.out.println("The current LinkedList is: ");
+            obj.showLinkedList();
+        }
+    }
+
